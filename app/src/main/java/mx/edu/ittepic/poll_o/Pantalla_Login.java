ab1d855bde5a1ab18631c37dad2e3bcd9943a57c
NetoEspeciano <especiano94@gmail.com>
2016-05-19 12:45:41 -0600

    Pantalla Login, recuerde que marca error en el manifest si tienen una pantalla nueva que no han subido. borren la linea diferente ( obvio copiada) luego hagan el pull y ahora si push

---
+++ app/src/main/java/mx/edu/ittepic/poll_o/Pantalla_Login.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.poll_o;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla__login);
+    }
+}
