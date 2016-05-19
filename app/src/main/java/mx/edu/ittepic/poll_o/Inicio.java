40a248b44c3fa90aa7d23ffa0197a35dd2749a37
Ernesto Pacheco Morelos <erpachecomo@ittepic.edu.mx>
2016-05-19 16:25:32 -0600

    Splash agregado

--- app/src/main/java/mx/edu/ittepic/poll_o/Inicio.java
+++ app/src/main/java/mx/edu/ittepic/poll_o/Inicio.java
@@ -1,14 +1,52 @@
 package mx.edu.ittepic.poll_o;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
 
 public class Inicio extends AppCompatActivity {
-    int prueba1github;
+    ImageView letra, logo;
+    CountDownTimer timer;
+    float transparencia_letras;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_inicio);
+        letra = (ImageView) findViewById(R.id.ivletras);
+        logo = (ImageView) findViewById(R.id.ivlogo);
+        transparencia_letras=0;
+
+        letra.setAlpha(transparencia_letras);
+        timer = new CountDownTimer(10000,10) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                transparencia_letras+=0.09;
+                letra.setAlpha(transparencia_letras);
+                if(transparencia_letras>=0.5){
+                    logo.setImageBitmap(BitmapFactory.decodeResource(Inicio.this.getResources(), R.drawable.logopollo));
+                }
+                if(transparencia_letras>=1){
+                    timer.cancel();
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                if(transparencia_letras>=1){
+                    startActivity(new Intent(Inicio.this,Pantalla_Login.class));
+                }else{
+                    timer.start();
+                }
+            }
+        };
+
+        timer.start();
     }
 }
+
