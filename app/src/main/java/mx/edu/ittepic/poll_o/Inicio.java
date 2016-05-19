ca407763142c5c4927d15e6985a2f227440f4998
NetoEspeciano <especiano94@gmail.com>
2016-05-19 11:31:24 -0600

    Prueba 1

--- app/src/main/java/mx/edu/ittepic/poll_o/Inicio.java
+++ app/src/main/java/mx/edu/ittepic/poll_o/Inicio.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Inicio extends AppCompatActivity {
-
+    int prueba1github;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
