cc2f2a6272be71395e7d43d02e7144f424ef4d2f
Ernesto Pacheco Morelos <erpachecomo@ittepic.edu.mx>
2016-05-19 22:30:52 -0600

    Splash actualizado con SurfaceView añadidos permisos en el manifest

--- app/src/main/java/mx/edu/ittepic/poll_o/Inicio.java
+++ app/src/main/java/mx/edu/ittepic/poll_o/Inicio.java
@@ -1,58 +1,143 @@
 package mx.edu.ittepic.poll_o;
 
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.Window;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class Inicio extends AppCompatActivity {
-    ImageView letra, logo;
-    CountDownTimer timer;
-    float transparencia_letras;
 
+    Point tamanoPantalla;
+    Bitmap letras, logo;
+    int alfa;
+    Lienzo lienzo;
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_inicio);
-        letra = (ImageView) findViewById(R.id.ivletras);
-        logo = (ImageView) findViewById(R.id.ivlogo);
-        transparencia_letras=0;
+        lienzo=new Lienzo(this);
+        setContentView(lienzo);
+        alfa=0;
 
-        letra.setAlpha(transparencia_letras);
-        timer = new CountDownTimer(10000,10) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                transparencia_letras+=0.09;
-                letra.setAlpha(transparencia_letras);
-                if(transparencia_letras>=0.5){
-                    logo.setImageBitmap(BitmapFactory.decodeResource(Inicio.this.getResources(), R.drawable.logopollo));
-                }
-                if(transparencia_letras>=1){
-                    timer.onFinish();
-                }
-            }
+        tamanoPantalla = new Point();
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getSize(tamanoPantalla);
 
-            @Override
-            public void onFinish() {
-                if(transparencia_letras>=1){
+        logo = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.logosinguino),tamanoPantalla.x / 3,tamanoPantalla.x / 3,false);
+        Bitmap letraspollo=BitmapFactory.decodeResource(getResources(), R.drawable.letraspollo);
+        letras = Bitmap.createScaledBitmap(letraspollo,letraspollo.getWidth() /3,letraspollo.getHeight()/3,false);
 
-                    Intent pantalla= new Intent(Inicio.this,Pantalla_Login.class);
-                    startActivity(pantalla);
-
-                }else{
-                    timer.start();
-
-                }
-            }
-        };
-
-        timer.start();
     }
-}
 
+    public class Lienzo extends SurfaceView implements SurfaceHolder.Callback{
+        Hilo h;
+        public Lienzo(Context c){
+            super(c);
+            getHolder().addCallback(this);
+        }
+
+        public boolean onTouchEvent(MotionEvent e){
+            return true;
+        }
+
+        public void onDraw(Canvas c) {
+            try{
+                Paint p = new Paint();
+                c.drawColor(Color.WHITE);
+                p.setAlpha(alfa);
+                c.drawBitmap(letras, tamanoPantalla.x / 2 - letras.getWidth() / 2, tamanoPantalla.y / 5, p);
+                p.setAlpha(255);
+                c.drawBitmap(logo,tamanoPantalla.x/2-logo.getWidth()/2,tamanoPantalla.y/4+letras.getHeight(),p);
+            }catch(Exception e){}
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            h = new Hilo(this);
+            h.start();
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            h.repetir=false;
+            try {
+                h.join();
+            } catch (InterruptedException e) {
+
+            }
+        }
+
+    }
+    public class Hilo extends Thread{
+        boolean repetir;
+        Lienzo puntero;
+        int sleep;
+        boolean aumentoAlfa;
+        public Hilo(Lienzo ref){
+            repetir=true;
+            aumentoAlfa=true;
+            puntero=ref;
+            sleep=50;
+        }
+        public void run(){
+            Canvas canvas=null;
+            while(repetir){
+                if(sleep==1500){
+                    repetir=false;
+                }
+                //Aquí 2do plano
+                try{
+                    try {
+                        sleep(sleep);
+                        if(aumentoAlfa){
+                            alfa+=5;
+                        }else{
+                            logo = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.logopollo), tamanoPantalla.x / 3, tamanoPantalla.x / 3, false);
+                            sleep=1500;
+                        }
+                        if(alfa>=255){
+                            aumentoAlfa=false;
+                            alfa=255;
+                        }
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    canvas = puntero.getHolder().lockCanvas();
+                    synchronized (puntero.getHolder()){
+                        puntero.onDraw(canvas);
+                    }
+                }finally{
+                    //Se ejecuta cuando el recurso se libera
+                    if(canvas!=null) {
+                        puntero.getHolder().unlockCanvasAndPost(canvas);
+                    }
+                }
+            }
+            puntero.getContext().startActivity(new Intent(Inicio.this,Pantalla_Login.class));
+        }
+    }
+
+}
