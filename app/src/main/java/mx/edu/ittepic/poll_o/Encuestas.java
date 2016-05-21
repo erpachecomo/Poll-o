494f1d6b6176cd0865516d00a299f147e6b278c6
NetoEspeciano <especiano94@gmail.com>
2016-05-21 12:35:42 -0600

    estoy enlazando en la APP

--- app/src/main/java/mx/edu/ittepic/poll_o/Encuestas.java
+++ app/src/main/java/mx/edu/ittepic/poll_o/Encuestas.java
@@ -1,13 +1,33 @@
 package mx.edu.ittepic.poll_o;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Encuestas extends AppCompatActivity {
-
+    ListView encuestasdisponibles;
+    String encuesta_seleccionada;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encuestas);
+        encuesta_seleccionada="";
+
+        encuestasdisponibles=(ListView)findViewById(R.id.encuestas_disponibles);
+        encuestasdisponibles.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                encuesta_seleccionada=encuestasdisponibles.getItemAtPosition(position).toString();
+
+                Intent PantallaMenuEncuesta=new Intent(Encuestas.this,MenuEncuesta.class);
+                PantallaMenuEncuesta.putExtra("seleccion",encuesta_seleccionada);
+                startActivity(PantallaMenuEncuesta);
+
+            }
+        });
     }
 }
--- app/src/main/java/mx/edu/ittepic/poll_o/MenuEncuesta.java
+++ app/src/main/java/mx/edu/ittepic/poll_o/MenuEncuesta.java
@@ -2,12 +2,25 @@ package mx.edu.ittepic.poll_o;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MenuEncuesta extends AppCompatActivity {
+    String encuesta_a_Realizar;
+    TextView Encuesta;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_encuesta);
+        encuesta_a_Realizar=getIntent().getStringExtra("seleccion");
+        Encuesta=(TextView)findViewById(R.id.Encuesta_a_Realizar);
+        Encuesta.setText(encuesta_a_Realizar);
+
     }
+
+
 }
