deccb8ae5d0af992f75f30a11b254c27a07a8709
Ernesto Pacheco Morelos <erpachecomo@ittepic.edu.mx>
2016-05-31 19:46:17 -0600

    Login con SMS V1

--- app/src/main/java/mx/edu/ittepic/poll_o/Pantalla_Login.java
+++ app/src/main/java/mx/edu/ittepic/poll_o/Pantalla_Login.java
@@ -1,13 +1,18 @@
 package mx.edu.ittepic.poll_o;
 
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +20,7 @@ import android.widget.Toast;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.Principal;
 import java.util.ArrayList;
 
 public class Pantalla_Login extends AppCompatActivity {
@@ -22,11 +28,10 @@ public class Pantalla_Login extends AppCompatActivity {
     EditText Campo_Usuario,Campo_Contra;
     Button Ini_Sesion;
     String Usuario,Contrasena;
-
-
-
+    BroadcastReceiver recibidorMensajesSMS;
     VerificarConexionWIFI verificadorConexion;
     boolean sesion_valida;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +43,20 @@ public class Pantalla_Login extends AppCompatActivity {
         sesion_valida=false;
         conexion=new ConexionBD(this,"Poll-oB2",null,1);
         Usuario=Contrasena="";
+        final DialogInterface.OnClickListener di = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface d, int which){
+                switch(which){
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        d.cancel();
+                        break;
+                    case DialogInterface.BUTTON_POSITIVE:
+                        IniciarSesionSMS(Usuario, Contrasena);
+                        d.dismiss();
+                        break;
+                }
+            }
+
+        };
         Ini_Sesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,7 +69,13 @@ public class Pantalla_Login extends AppCompatActivity {
                         IniciarSesion(Usuario, Contrasena);
 
                     } else {
-                        //AQUI DEBE IR EL CODIGO PARA MANDAR UN MJS AL SERVIDOR SI NO HAY INTERNET
+                        AlertDialog.Builder alert = new AlertDialog.Builder(Pantalla_Login.this);
+                        alert.setTitle("Login");
+                        alert.setMessage("No tienes una conexión a internet, puedes ingresar enviando un SMS lo cual generará costos con tu operadora, estás seguro?");
+                        alert.setPositiveButton("Si", di);
+                        alert.setNegativeButton("No", di);
+                        alert.show();
+
                     }
                             /*-------------------------------------*/
                 } else {
@@ -58,8 +83,40 @@ public class Pantalla_Login extends AppCompatActivity {
                 }
             }
         });
-    }
+        IntentFilter evento = new IntentFilter("android.provider.Telephony.SMS_RECEIVED"); //Cuando entre un mensaje nuevo;
+        recibidorMensajesSMS = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Bundle b = intent.getExtras();
 
+                Object mensajeObjeto[] = (Object[]) b.get("pdus");//Esto es obtener el mensaje entrante.
+
+                SmsMessage mensaje = SmsMessage.createFromPdu((byte[])mensajeObjeto[0]);//),"UTF-8");
+                if(mensaje.getDisplayOriginatingAddress().equals("3111349397")){
+                if(mensaje.getMessageBody().equals("si")){
+                    Intent MenuPrincipal = new Intent(Pantalla_Login.this, mx.edu.ittepic.poll_o.MenuPrincipal.class);
+                    MenuPrincipal.putExtra("Usuario", Usuario);
+                    startActivity(MenuPrincipal);
+                }if(mensaje.getMessageBody().equals("cliente")){
+                    ActualizarBaseDeDatos();
+                    //Toast.makeText(Pantalla_Login.this,"Inicio correcto",Toast.LENGTH_SHORT).show();
+                    Intent Encuestas= new Intent(Pantalla_Login.this, mx.edu.ittepic.poll_o.Encuestas.class);
+                    Encuestas.putExtra("Usuario",Usuario);
+                    Encuestas.putExtra("Tipo","1");
+                    startActivity(Encuestas);
+                    //super.onResume();
+                }
+            }
+                Toast.makeText(Pantalla_Login.this, "Celular"+mensaje.getDisplayOriginatingAddress()+"Mensaje"+mensaje.getMessageBody(), Toast.LENGTH_SHORT).show();
+            }
+        };
+        registerReceiver(recibidorMensajesSMS, evento);
+    }
+    private void IniciarSesionSMS(String usuario, String contrasena) {
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage("3111349397", null, "Poll-o,"+usuario+","+contrasena, null, null);//telefono, provedor de servicios,Activity de envio:Cuando lo envía manda a llamr a un activity, Activity de recepcion:cuando llega el sms
+        Toast.makeText(Pantalla_Login.this, "Iniciando sesión vía SMS", Toast.LENGTH_SHORT).show();
+    }
     private void IniciarSesion(String usuario, String contrasena) {
         try {
             ConexionWeb web = new ConexionWeb(Pantalla_Login.this,0);
@@ -83,7 +140,6 @@ public class Pantalla_Login extends AppCompatActivity {
             //Toast.makeText(Pantalla_Login.this, "Inicio correcto", Toast.LENGTH_LONG).show();
             Intent MenuPrincipal = new Intent(Pantalla_Login.this, mx.edu.ittepic.poll_o.MenuPrincipal.class);
             MenuPrincipal.putExtra("Usuario", Usuario);
-
             startActivity(MenuPrincipal);
         }
         if(Respuesta.equals("cliente")){
